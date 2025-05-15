30c349640d9d263127fa0b5ec94af7dd8baf5734
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-05-14 22:23:08 -0600

    feat(bill): add prices to bill items

--- ward/src/main/java/org/ehealth/ward/domain/dto/ward/admission/AddAdmissionDto.java
+++ ward/src/main/java/org/ehealth/ward/domain/dto/ward/admission/AddAdmissionDto.java
@@ -4,14 +4,12 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder(toBuilder = true)
 public record AddAdmissionDto(
-        @JsonFormat(pattern = "yyyy-MM-dd") @NotNull @FutureOrPresent LocalDate admissionDate,
-        @JsonFormat(pattern = "yyyy-MM-dd") @Future LocalDate dischargeDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") @NotNull LocalDate admissionDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate dischargeDate,
         Long roomId) {
 }
